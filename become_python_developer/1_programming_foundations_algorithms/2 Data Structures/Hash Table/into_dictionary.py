9872a3b79b61344d5a3b594669a2e6bd453858ec
Gustavo Colmenares <gcolmenaresr@gmail.com>
2020-01-12 22:54:42 -0800

    adding pratice dictionary file

---
+++ become_python_developer/1_programming_foundations_algorithms/2 Data Structures/Hash Table/into_dictionary.py
@@ -0,0 +1,24 @@
+# practice with dictionaries
+
+# create a dictionary
+
+# Create 3 employee and with name, age, position.
+
+employee = {}
+
+
+
+# add to the dictionary employee: salary, race
+
+
+# change values from the employee dict
+
+
+# delete values from employee dict 
+
+
+# delete employee from employeee dict
+
+
+
+# create function to automate all previous steps
