27e4f1492adaded85996a3d75dc3d7601afc284f
Gustavo Colmenares <gcolmenaresr@gmail.com>
2020-01-13 05:40:39 -0800

    updating pratice dictionary file

--- become_python_developer/1_programming_foundations_algorithms/2 Data Structures/Hash Table/into_dictionary.py
+++ become_python_developer/1_programming_foundations_algorithms/2 Data Structures/Hash Table/into_dictionary.py
@@ -1,13 +1,36 @@
 # practice with dictionaries
 
-# create a dictionary
-
-# Create 3 employee and with name, age, position.
-
+# create a dictionary an empty dict
 employee = {}
 
+# add 3 employee and with name, age, position.
+employee['Gustavo'] = [36, 'gis']
+employee['Monica'] = [30, 'accountant']
+employee['Jessica']= [32, 'teacher']
 
 
+# Create the same dictionary at ones 
+
+# Option 1
+# employee = {'Gustavo': [36, 'gis'], 'Monica':[30, 'accountant'], 'Jessica':[32, 'teacher']}
+
+# Option 2
+# employee = dict(Gustavo = [36, 'gis'], Monica = [30, 'accountant'], Jessica =[32, 'teacher'])
+
+
+# return the Gustavo's age
+# print(employee['Gustavo'][0]) 
+
+# Change age for Jessica
+employee['Jessica'][0] = 30
+
+# Iterate through the dictionary and return all the keys & values 
+# for key , value in employee.items():
+# 	print('key:')
+# 	print(key)
+# 	print('value:')
+# 	print(value)
+# 	print('')
 # add to the dictionary employee: salary, race
 
 
@@ -20,5 +43,17 @@ employee = {}
 # delete employee from employeee dict
 
 
+#----------- How to handle dictionary errors ----------------
 
-# create function to automate all previous steps
+''' 
+let's try to find a key that does not exist in the employee dict using the get() method
+'''
+# print(employee['Tom']) # with this you will get an error because this key does not exist in dict
+
+# to return a None value 
+# print(employee.get('Tom')) 
+
+# lets define the message in return:
+# print(employee.get('Tom', 'Not Found'))
+
+
